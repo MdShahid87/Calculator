98640ebf7bec3d1b4fdcb891b0a488c302ce5c3b
Md Shahid <shahidaarzoo28@gmail.com>
2025-10-14 20:29:58 +0530

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,28 +1,30 @@
-let input=document.getElementById('inputBox');
-let buttons=document.querySelector('button')
+let inputBox=document.getElementById("inputbox");
+let buttons=document.querySelectorAll('button')
 
-let  string ="";
-let arr=Array.from(buttons);
-arr.forEach(button=>{
-    button.addEventListener('clcik',(e)=>{
-         if(e.target.innerHTML=='='){
-            string=eval(string);
-            input.value=string;
-         }
 
-         else if(e.target.innerHTML=='AC'){
-            string ="";
-            input.value=string;
-         }
-         else if(e.target.innerHTML=="DEL"){
-             string=string.substring(0,string.length-1);
-             input.value=string;
-         }
-         else{
-            string+=e.target.innerHTML;
-            input.value=string;
-         }
-    })
-     
+let string=''
+
+buttons.forEach(element=>{
+   element.addEventListener('click',(b)=>{
+      if(b.target.innerText=='='){
+         string =String(eval(string))
+         inputBox.value=string;
+      }
+       else if(b.target.innerText=='AC'){
+         string=''
+         inputBox.value=string;
+      }
+       else if(b.target.innerText=='DEL'){
+        string=string.substring(0,string.length-1)
+        inputBox.value=string
+       }
+       else if(b.target.id=='plusMinus'){
+         string=string(-eval(string))
+         inputBox.value=string
+       }
+      else{
+         string +=b.target.innerText
+         inputBox.value=string;
+      }
+   })
 })
-
